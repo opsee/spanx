19d51dd88487667ed2aab540d38c9741bd1290bf
Mark Martin <assplecake@gmail.com>
2016-03-08 14:32:21 -0800

    update opsee role to allow it to update itself

--- src/github.com/opsee/spanx/roler/policies.go
+++ src/github.com/opsee/spanx/roler/policies.go
@@ -42,57 +42,64 @@ const (
 }
 `
 	Policy = `{
- "Version": "2012-10-17",
- "Statement": [
-  {
-   "Effect": "Allow",
-   "Action": [
-    "autoscaling:DescribeLoadBalancers",
-    "autoscaling:DescribeAutoScalingGroups",
-    "cloudformation:CreateStack",
-    "cloudformation:DeleteStack",
-    "cloudformation:DescribeStacks",
-    "cloudformation:DescribeStackResources",
-    "cloudformation:ListStackResources",
-    "cloudformation:UpdateStack",
-    "ec2:CreateTags",
-    "ec2:DeleteTags",
-    "ec2:AuthorizeSecurityGroupIngress",
-    "ec2:AuthorizeSecurityGroupEgress",
-    "ec2:RevokeSecurityGroupIngress",
-    "ec2:RevokeSecurityGroupEgress",
-    "ec2:StartInstances",
-    "ec2:RunInstances",
-    "ec2:StopInstances",
-    "ec2:RebootInstances",
-    "ec2:TerminateInstances",
-    "ec2:DescribeAccountAttributes",
-    "ec2:DescribeImages",
-    "ec2:DescribeSecurityGroups",
-    "ec2:CreateSecurityGroup",
-    "ec2:DeleteSecurityGroup",
-    "ec2:DescribeSubnets",
-    "ec2:DescribeVpcs",
-    "ec2:DescribeInstances",
-    "ec2:DescribeInternetGateways",
-    "ec2:DescribeRouteTables",
-    "elasticloadbalancing:DescribeLoadBalancers",
-    "sns:CreateTopic",
-    "sns:DeleteTopic",
-    "sns:Subscribe",
-    "sns:Unsubscribe",
-    "sns:Publish",
-    "sqs:CreateQueue",
-    "sqs:DeleteQueue",
-    "sqs:DeleteMessage",
-    "sqs:ReceiveMessage",
-    "sqs:GetQueueAttributes",
-    "sqs:SetQueueAttributes",
-    "rds:DescribeDBInstances",
-    "rds:DescribeDBSecurityGroups"
-   ],
-   "Resource": "*"
-  }
- ]
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "autoscaling:DescribeLoadBalancers",
+        "autoscaling:DescribeAutoScalingGroups",
+        "cloudformation:CreateStack",
+        "cloudformation:DeleteStack",
+        "cloudformation:DescribeStacks",
+        "cloudformation:DescribeStackResources",
+        "cloudformation:ListStackResources",
+        "cloudformation:UpdateStack",
+        "ec2:CreateTags",
+        "ec2:DeleteTags",
+        "ec2:AuthorizeSecurityGroupIngress",
+        "ec2:AuthorizeSecurityGroupEgress",
+        "ec2:RevokeSecurityGroupIngress",
+        "ec2:RevokeSecurityGroupEgress",
+        "ec2:StartInstances",
+        "ec2:RunInstances",
+        "ec2:StopInstances",
+        "ec2:RebootInstances",
+        "ec2:TerminateInstances",
+        "ec2:DescribeAccountAttributes",
+        "ec2:DescribeImages",
+        "ec2:DescribeSecurityGroups",
+        "ec2:CreateSecurityGroup",
+        "ec2:DeleteSecurityGroup",
+        "ec2:DescribeSubnets",
+        "ec2:DescribeVpcs",
+        "ec2:DescribeInstances",
+        "ec2:DescribeInternetGateways",
+        "ec2:DescribeRouteTables",
+        "elasticloadbalancing:DescribeLoadBalancers",
+        "sns:CreateTopic",
+        "sns:DeleteTopic",
+        "sns:Subscribe",
+        "sns:Unsubscribe",
+        "sns:Publish",
+        "sqs:CreateQueue",
+        "sqs:DeleteQueue",
+        "sqs:DeleteMessage",
+        "sqs:ReceiveMessage",
+        "sqs:GetQueueAttributes",
+        "sqs:SetQueueAttributes",
+        "rds:DescribeDBInstances",
+        "rds:DescribeDBSecurityGroups"
+      ],
+      "Resource": "*"
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "iam:*"
+      ],
+      "Resource": "arn:aws:iam::*:role/opsee-role-*"
+    }
+  ]
 }`
 )
